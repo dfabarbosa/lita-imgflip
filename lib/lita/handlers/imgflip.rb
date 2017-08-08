4ffc7c00ffee7cea009882c2b33bba276d571569
Daniel Barbosa <dfabarbosa@gmail.com>
2017-08-08 17:12:06 +0100

    sara meme

--- lib/lita/handlers/imgflip.rb
+++ lib/lita/handlers/imgflip.rb
@@ -22,6 +22,7 @@ module Lita
       route %r{(one does not simply) (.*)}i,                       :meme_simply,             help: { "one does not simply.." => "generates one does not simply.. meme" }
       route %r{(grumpy cat) (.*),(.*)}i,                           :meme_grumpy_cat,         help: { "grumpy cat .. , .." => "generates grumpy cat .. , .. meme" }
       route %r{(pedro_meme) (.*),(.*)}i,                           :meme_pedro_meme,         help: { "pedro_meme .. , .." => "generates pedro .. , .. meme" }
+      route %r{(sara_meme) (.*),(.*)}i,                            :meme_sara_meme,          help: { "sara_meme .. , .." => "generates sara .. , .. meme" }
       route %r{(ang20_meme) (.*)}i,                                :meme_ang20_meme,         help: { "ang20_meme.." => "generates angela coelho .. meme" }
       # route %r{(it looks like you're|it looks like you) (.*)}i,    :meme_looks_like,         help: { "it looks like you're .." => "generates it looks like you're .. meme", "it looks like you.." => "(case insensitive) generates it looks like you.. meme" }
       route %r{(AM I THE ONLY ONE AROUND HERE) (.*)}i,             :meme_am_i_only,          help: { "AM I THE ONLY ONE AROUND HERE.." => "generates AM I THE ONLY ONE AROUND HERE.. meme" }
@@ -97,6 +98,10 @@ module Lita
 
       def meme_pedro_meme(response)
         generate_meme(response, 109522183, text0:response.matches[0][1], text1:response.matches[0][2])
+      end
+
+      def meme_sara_meme(response)
+        generate_meme(response, 110461590, text0:response.matches[0][1], text1:response.matches[0][2])
       end
 
       # def meme_looks_like(response)
